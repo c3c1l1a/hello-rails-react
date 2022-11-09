43a6a6fde558fd288f9793cb85731903dce82a65
c3c1l1a <cesswangumukima@gmail.com>
2022-11-09 19:43:05 +0800

    Add a slice for all messages

--- app/javascript/redux/greetings.js
+++ app/javascript/redux/greetings.js
@@ -2,9 +2,25 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 
 const fetchAllMessages = createAsyncThunk(
-	'greetings/fetchAll',
+	'greetings/All',
 	async ('', thunkAPI) => {
 		const response = await fetch('api/v1/messages/index')
 		return response.data
 	}
 );
+
+const initialState = {
+  entities: [],
+  loading: 'idle',
+} as MessagesState
+
+
+const messagesSlice = createSlice({
+	name: 'messages',
+	initialState,
+	extraReducers: (builder) => {
+		builder.addCase(fetchAllMessages.fulfilled, (state, action) => {
+			state.entities.push(action.payload)
+		})
+	}
+});

45abee26dafba8685d0b934433e08285c99806df
c3c1l1a <cesswangumukima@gmail.com>
2022-11-10 12:59:11 +0800

    Add the messages route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "root#index"
+
+  namespace :api do
+    namespace :v1 do 
+      resources :messages
+    end
+  end
 end

847e57d95675c2b82ba7a59d53431043c74c0366
c3c1l1a <cesswangumukima@gmail.com>
2022-11-11 13:01:44 +0800

    Remove linter errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,12 @@
-class Api::V1::MessagesController < ApplicationController
-  def index
-    @message = Message.random
-    render json: @message
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class MessagesController < ApplicationController
+      def index
+        @message = Message.random
+        render json: @message
+      end
+    end
   end
 end
